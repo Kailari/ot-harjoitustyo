f4298994ce77cf19464ed55514c52a8bcd98b554
toilari <ilari97@hotmail.com>
2018-11-02 01:56:17 +0200

    Lisättiin maven-projekti

---
+++ src/main/java/toilari/otlite/Launcher.java
@@ -0,0 +1,11 @@
+package toilari.otlite;
+
+/**
+ * Application launcher. Main entry point for the application.
+ */
+public class Launcher {
+    public static void main(String[] args) {
+        OTLite app = new OTLite();
+        app.run();
+    }
+}
---
+++ src/main/java/toilari/otlite/OTLite.java
@@ -0,0 +1,10 @@
+package toilari.otlite;
+
+/**
+ * Application main class. Contains methods necessary for running the game.
+ */
+public class OTLite {
+	public void run() {
+		System.out.println("Hello OT!");
+	}
+}
