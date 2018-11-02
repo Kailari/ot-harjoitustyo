362de7406c0648b8714857897d532fc5b1a24a21
toilari <ilari97@hotmail.com>
2018-11-02 21:05:21 +0200

    Siivottiin importit

--- src/test/java/toilari/otlite/io/util/FileHelperTest.java
+++ src/test/java/toilari/otlite/io/util/FileHelperTest.java
@@ -2,15 +2,10 @@ package toilari.otlite.io.util;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.junit.jupiter.api.Assertions.fail;
 
-import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.io.Reader;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
