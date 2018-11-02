8e4395283b61d928e5e5fae91275e54f40272d72
toilari <ilari97@hotmail.com>
2018-11-02 04:43:05 +0200

    Lisättiin JavaDoc-kommentit

--- src/main/java/toilari/otlite/Launcher.java
+++ src/main/java/toilari/otlite/Launcher.java
@@ -4,6 +4,11 @@ package toilari.otlite;
  * Vastaa sovelluksen käynnistämisestä ja komentoriviparametrien parsimisesta.
  */
 public class Launcher {
+    /**
+     * Main-metodi, parsii komentoriviparametrit ja käynnistää pelin.
+     * 
+     * @param args Raa'at, parsimattomat kometoriviparametrit
+     */
     public static void main(String[] args) {
         OTLite app = new OTLite();
         app.run();
