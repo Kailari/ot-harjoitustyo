7c31effcaf3d24495235bfeee936a0714eb698d9
toilari <ilari97@hotmail.com>
2018-11-02 20:59:10 +0200

    Cleaned up imports

--- src/test/java/toilari/otlite/io/util/TextFileHelperTest.java
+++ src/test/java/toilari/otlite/io/util/TextFileHelperTest.java
@@ -9,7 +9,6 @@ import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.nio.file.StandardOpenOption;
 import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
