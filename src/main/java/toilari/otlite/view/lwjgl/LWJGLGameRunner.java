de25e042dd28d7c42eed3bfef52916feb2d2af4c
toilari <ilari97@hotmail.com>
2018-11-22 01:39:13 +0200

    Ikkunan kokoa ei enää voi muuttaa

--- src/main/java/toilari/otlite/view/lwjgl/LWJGLGameRunner.java
+++ src/main/java/toilari/otlite/view/lwjgl/LWJGLGameRunner.java
@@ -94,7 +94,7 @@ public class LWJGLGameRunner extends AbstractGameRunner<LWJGLCamera> {
 
     private void initWindow() {
         glfwWindowHint(GLFW_VISIBLE, GLFW_FALSE);
-        glfwWindowHint(GLFW_RESIZABLE, GLFW_TRUE);
+        glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
         glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
         glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
         glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
